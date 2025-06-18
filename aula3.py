c62c4d750b6b8e00b9688693636e0c1ded1d1eff
Ducher M Seidi <ducherseide@gmail.com>
2025-06-18 11:06:37 -0300

    variavel

---
+++ aula3.py
@@ -0,0 +1,24 @@
+# Introduzindo variavel
+
+nome_completo = "Ducher M Seidi"
+idade = 25
+altura = 1.92
+peso = 80.5
+print(f"Nome: {nome_completo}\nIdade: {idade}\nAltura: {altura}\nPeso: {peso}")
+
+maior_de_idade = idade >= 18
+print(f"É maior de Idade? {maior_de_idade}\n\n")
+
+# Vamos fazer algum exercícios
+
+nome_completo2 = "Alex Brasileiro"
+idade2 = 30
+altura2 = 1.75
+peso2 = 70.0
+ciade_natal = "Bula"
+ano_nasscimento = 2025 - idade2
+nacionalidade = "Guineense"
+print(f"Nome: {nome_completo2}\nIdade: {idade2}\nAltura: {altura2}\nPeso: {peso2}\nCidade Natal: {ciade_natal}\nAno de Nascimento: {ano_nasscimento}\nNacionalidade: {nacionalidade}")
+# Verificando se é maior de idade
+maior_de_idade2 = idade2 >= 18
+print(f"É maior de Idade? {maior_de_idade2}")
